3284a51e07bba3f5307cd2079fce99145b830c71
smit-joshi <smit-joshi>
2016-03-10 11:29:00 +0530

    finace page add calculator

--- view/finance/financeController.js
+++ view/finance/financeController.js
@@ -5,19 +5,26 @@ autoscityControllers.controller('financeController', ['$scope', '$window',
 
         $("#loader").fadeOut();
 
-        $scope.heading = "Landing Page...";
+        $scope.ratetotal = 0;
+        $scope.TotalRepayment = 0;
+        $scope.TotalCostofCredit = 0;
 
+        $scope.aprCalculation = function () {
 
+            var i = $scope.interest;
+            var q = $scope.month;
+            var m = $scope.modi;
 
-        /*getKeyList.get(function(response){
-            if(response.status == 0){
-                $scope.keylist = response.data;
-            }
-            else{
-                $.toaster(response.message, 'Alert', 'warning');
-            }
-        });*/
+            var sq = Math.pow((1+(i/q)), q);
+            $scope.ratetotal = sq-1;
+            console.log("APR : " +JSON.stringify($scope.ratetotal));
 
+            var cc = (($scope.ratetotal * m)* (q / 12));
+            $scope.TotalCostofCredit = cc;
+            console.log("CC : " +JSON.stringify($scope.TotalCostofCredit));
 
-
+            var rp =  m + $scope.TotalCostofCredit;
+            $scope.TotalRepayment = rp;
+            console.log("RP : " +JSON.stringify($scope.TotalRepayment));
+        }
     }]);
